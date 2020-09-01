f7eb1a015c94a31516d41ea8d03a6c28aabab3c7
Mehdi OUSFANE <64252905+mehdi-ousfane@users.noreply.github.com>
2020-09-02 01:08:53 +0200

    start for the container

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
+import classes from './Layout.module.css';
 import Aux from '../../HOC/Aux';
 
 const layout = (props) => (
     <Aux>
         <div>
-
+            <p>poulpoul</p>
         </div>
-        <main>
+        <main className={classes.Content}>
             {props.children}
         </main>
     </Aux>
---
+++ src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+import Aux from '../../HOC/Aux';
+
+class BurgerBuilder extends Component {
+    render() {
+        return(
+            <Aux>
+                <div>burger</div>
+            </Aux>
+        );
+    }
+}
+
+export default BurgerBuilder;
