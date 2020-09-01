aa2fbb6a46a0e155d2bd4751ffe4629539cbcac7
Mehdi OUSFANE <64252905+mehdi-ousfane@users.noreply.github.com>
2020-09-01 23:54:50 +0200

    layout and folders

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
+import Layout from './Components/Layout/Layout';
+
 function App() {
   return (
     <div>
+      <Layout>
+        <p>blablabla</p>
+      </Layout>
       
     </div>
   );
---
+++ src/Components/Layout/Layout.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Aux from '../../HOC/Aux';
+
+const layout = (props) => (
+    <Aux>
+        <div>
+
+        </div>
+        <main>
+            {props.children}
+        </main>
+    </Aux>
+);
+
+export default layout;
