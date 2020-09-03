3041dc213e1518a4fb68bc653230599bb38bb4e3
Mehdi OUSFANE <64252905+mehdi-ousfane@users.noreply.github.com>
2020-09-03 20:14:23 +0200

    ingredients and buildcontrol start

--- src/Components/Burger/Burger.js
+++ src/Components/Burger/Burger.js
@@ -4,11 +4,20 @@ import classes from './Burger.module.css';
 import BurgerIngredients from './BurgerIngredients/BurgerIngredients';
 
 const burger = (props) => {
+    let transIngredients = Object.keys(props.ingredients).map(ingrKey=>{ 
+        return [...Array(props.ingredients[ingrKey])].map((_,i)=>{
+            return <BurgerIngredients key={ingrKey+i} type={ingrKey} />
+        });
+    }).reduce((arr,el)=>{
+        return arr.concat(el)
+    },[]);
+    if (transIngredients === 0) {
+        transIngredients = <p>Add some ingredients ;')'</p>
+    }
     return(
         <div className={classes.Burger}>
             <BurgerIngredients type="bread-top" />
-            <BurgerIngredients type="cheese" />
-            <BurgerIngredients type="meat" />
+            {transIngredients}
             <BurgerIngredients type="bread-bottom" />
         </div>
     );
--- src/Containers/BurgerBuilder/BurgerBuilder.js
+++ src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -4,10 +4,23 @@ import Aux from '../../HOC/Aux';
 import Burger from '../../Components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+    constructor() {
+
+    }
+
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 1,
+            cheese: 1,
+            meat: 1
+        }
+    }
+
     render() {
         return(
             <Aux>
-                <Burger />
+                <Burger ingredients={this.state.ingredients} />
             </Aux>
         );
     }
