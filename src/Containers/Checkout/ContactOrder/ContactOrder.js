c9ddb30a4e74811a2f95d92079a64c1312c91897
Mehdi OUSFANE <64252905+mehdi-ousfane@users.noreply.github.com>
2020-09-24 23:18:31 +0200

    pass data

--- src/Containers/Checkout/ContactOrder/ContactOrder.js
+++ src/Containers/Checkout/ContactOrder/ContactOrder.js
@@ -2,25 +2,54 @@ import React, {Component} from 'react';
 
 import Button from '../../../Components/UI/Button/Button';
 import classes from './ContactOrder.module.css';
+import axios from '../../../axios-orders';
+import Spinner from '../../../Components/UI/Spinner/Spinner';
 
 class ContactOrder extends Component {
     state= {
         name: '',
         email: '',
-        adress: ''
+        adress: '',
+        loading: false
+    }
+
+    orderHandler = (event) => {
+        event.preventDefault();
+        this.setState({loading: true})
+        const order = {
+            price: this.props.price,
+            ingredients: this.props.ingredients,
+            customer: {
+                name: 'boulbou',
+                adress: 'chinatown',
+                country: 'france',
+                email: 'boul@boul.com'
+            },
+            delivery: 'pigeon voyageur'
+        }
+        axios.post('/orders.json', order).then(
+            res => {this.setState({loading: false});
+                    this.props.history.push('/')}
+        ).catch(
+            e => {this.setState({loading: false});}
+        );
     }
 
     render() {
-        
+        let form = (
+            <form>
+                <input className={classes.Input} type='text' name='name' placeholder='Your Name' />
+                <input className={classes.Input} type='text' name='email' placeholder='Your Email' />
+                <input className={classes.Input} type='text' name='adress' placeholder='Your Adress' />
+                <Button btnType='Success' clicked={this.orderHandler}>Confirm the order</Button>
+            </form>);
+        if(this.state.loading) {
+            form = <Spinner/>
+        }
         return(
             <div className={classes.ContactOrder}>
                 <h4>Please enter your Contact information here:</h4>
-                <form>
-                    <input className={classes.Input} type='text' name='name' placeholder='Your Name' />
-                    <input className={classes.Input} type='text' name='email' placeholder='Your Email' />
-                    <input className={classes.Input} type='text' name='adress' placeholder='Your Adress' />
-                    <Button btnType='Success'>Confirm the order</Button>
-                </form>
+                {form}
             </div>
         )
     }
