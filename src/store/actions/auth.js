e76387033de67ece9bd655a61a106bd26e490ae4
Mehdi OUSFANE <64252905+mehdi-ousfane@users.noreply.github.com>
2020-10-30 22:26:33 +0100

    Update auth.js

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,4 +1,5 @@
 import * as actionTypes from './actionTypes';
+import axios from 'axios';
 
 export const authStart = () => {
     return {
@@ -23,5 +24,19 @@ export const authFail = (e) => {
 export const auth = (email, password) => {
     return dispatch => {
         dispatch(authStart());
+        const authData = {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        };
+        axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAE9BKPZggTKoiOVfEHe59bg8Oz-4lpU9w', authData)
+        .then(res => {
+            console.log(res);
+            dispatch(authSuccess(res));
+        })
+        .catch(e =>{ 
+            console.log(e);
+            dispatch(authFail(e));
+            });
     };
 };
